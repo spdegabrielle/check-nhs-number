e40027a6c763b6c82ddfe77b5a1f0d4f9b85d26c
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2016-09-30 08:32:37 +0100

    Create info.rkt

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang setup/infotab
+(define version "0.1 alpha")
+  (define name "NHS Number Validator")
+  (define blurb
+    '("Only really useful for health systems in the UK"))
+  (define primary-file "nhs-number-check.ss")
+  (define categories '(misc))
+  
+  (define repositories '("4.x"))
